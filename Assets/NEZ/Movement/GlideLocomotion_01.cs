b6c3d41a39a680017c5221962cecee7d8867a5c3
Jovan Aleksandroski <93161757+JovanAleksandroski@users.noreply.github.com>
2025-07-01 18:43:10 +0200

    JASS project

---
+++ Assets/NEZ/Movement/GlideLocomotion_01.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class AutoHeadMove : MonoBehaviour
+{
+    public GameObject rigRoot;         // XR Rig или Player објект
+    public Transform headTransform;    // Камера (телефон/главата)
+    public float speed = 1.5f;
+
+    void Update()
+    {
+        if (rigRoot == null || headTransform == null) return;
+
+        Vector3 moveDirection = headTransform.forward;
+        moveDirection.y = 0f; // само хоризонтално движење според поглед
+        rigRoot.transform.position += moveDirection.normalized * speed * Time.deltaTime;
+    }
+}
